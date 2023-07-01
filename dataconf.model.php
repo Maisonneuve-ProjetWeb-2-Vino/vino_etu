6a453137766b9469d9709382c5381f355b5bab86
Carl Matteau-Pelletier <carlmatteau@gmail.com>
2023-07-01 15:59:25 -0400

    mettre à jour dataconf.model

--- dataconf.model.php
+++ dataconf.model.php
@@ -1,7 +1,6 @@
 <?php
-	// Remplir et renommer dataconf.php
-	define("BASEURL", "");
-
+	// Remplir et renommer en dataconf.php
+	define('RELATIVE_URI', '');  // dossier racine du site par rapport au dossier racine d'Apache, exemple: '/vino_etu/'
 	define('HOST', 'localhost');
 	define('USER', '');
 	define('PASSWORD', '');
