a1ec4905022f12606483131562103111265a4094
kekcardinal <103967680+kekcardinal@users.noreply.github.com>
2023-07-11 14:12:30 -0400

    Ajout navbar footer + titre page

--- dataconf.model.php
+++
@@ -1,8 +0,0 @@
-<?php
-	// Copier le fichier et renommer en dataconf.php, puis remplir
-	define('RELATIVE_URI', '');  // dossier racine du site par rapport au dossier racine d'Apache, exemple: '/vino_etu/'
-	define('HOST', 'localhost');
-	define('USER', '');
-	define('PASSWORD', '');
-	define('DATABASE', '');
-?>
