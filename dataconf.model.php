49208c28db52cb5e4901f0ad8cde94e084fb3e00
Carl Matteau-Pelletier <carlmatteau@gmail.com>
2023-07-12 12:10:36 -0400

    re-mise de dataconf.model.php

---
+++ dataconf.model.php
@@ -0,0 +1,8 @@
+<?php
+	// Copier le fichier et renommer en dataconf.php, puis remplir
+	define('RELATIVE_URI', '');  // Dossier racine du site par rapport au dossier racine d'Apache, exemple: '/vino_etu/'
+	define('HOST', 'localhost');
+	define('USER', 'root'); // Usager base de donnée
+	define('PASSWORD', '');	// Mot de passe
+	define('DATABASE', ''); // Nom de la base de donnée
+?>
